859078421cc84ed8995b2d491568704ada8d83c8
Snehashis <catchlui@gmail.com>
2026-01-31 20:39:01 +0530

    storage

--- src/clean_corpus/storage/__init__.py
+++ src/clean_corpus/storage/__init__.py
@@ -13,3 +13,18 @@ __all__ = [
     "S3StorageBackend",
     "get_storage_backend",
 ]
+"""Storage helpers for local and S3 backends."""
+
+from .base import (
+    StorageBackend,
+    LocalStorageBackend,
+    S3StorageBackend,
+    get_storage_backend,
+)
+
+__all__ = [
+    "StorageBackend",
+    "LocalStorageBackend",
+    "S3StorageBackend",
+    "get_storage_backend",
+]
